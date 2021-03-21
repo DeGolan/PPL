a246c87713370047a2a33f5fbaa2e685e01544d9
gomerBGU <63105570+gomerBGU@users.noreply.github.com>
2021-03-21 18:41:56 +0200

    part2

--- Assignments/Assignment1/src/part2/part2.ts
+++ Assignments/Assignment1/src/part2/part2.ts
@@ -3,13 +3,46 @@ import * as R from "ramda";
 const stringToArray = R.split("");
 
 /* Question 1 */
-export const countVowels = (str) => {
-  const stringArr = stringToArray(str);
-  return stringArr.reduce((acc,curr)=>)
+export const countVowels : (str: string) => number = (str: string)=>{
+  const strAsArray: string[] = stringToArray(str);
+  const vowels: string="aeiouAEIOU";
+  return strAsArray.reduce((
+    (acc :number, curr: string) =>
+     vowels.indexOf(curr) != -1 ? acc + 1 : acc),
+     0
+     );
 };
-
+console.log(countVowels("This is SOME Text"));
 /* Question 2 */
-export const runLengthEncoding = undefined;
+export const runLengthEncoding : (str :string) => string = (str: string)=>{
+  const strAsArray: string[] = stringToArray(str); 
+  let counter: number = 0; //need to change
+  const newStr:string= strAsArray.reduce((
+    (acc :string, curr: string) =>
+      {
+        if(curr==acc.charAt(acc.length-1)){
+          counter=counter+1;
+          return acc;
+        }
+        else{
+          acc=acc+counter+curr;
+          counter=1;
+          return acc;
 
+        }
+      }),
+      strAsArray[0]
+     );
+  return counter>1 ? newStr+counter: newStr;
+};
+console.log(runLengthEncoding("aaaabbbccdd"));
 /* Question 3 */
-export const isPaired = undefined;
+export const isPaired :(str: string)=>boolean = (str:string)=>{
+  const strAsArray: string[] = stringToArray(str); 
+  const parentheses:string="(){}[]";
+  const filteredArray = strAsArray.filter(
+    (curr: string)=> parentheses.indexOf(curr) != -1
+  );
+  console.log(filteredArray);
+  return false;
+};
