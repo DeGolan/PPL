2dda49e0099e251ad929c729ef12c8066b7457c8
gomerBGU <63105570+gomerBGU@users.noreply.github.com>
2021-03-21 18:48:14 +0200

    amit's work

--- Assignments/Assignment1/src/part2/part2.ts
+++ Assignments/Assignment1/src/part2/part2.ts
@@ -3,51 +3,6 @@ import * as R from "ramda";
 const stringToArray = R.split("");
 
 /* Question 1 */
-<<<<<<< HEAD
-export const countVowels : (str: string) => number = (str: string)=>{
-  const strAsArray: string[] = stringToArray(str);
-  const vowels: string="aeiouAEIOU";
-  return strAsArray.reduce((
-    (acc :number, curr: string) =>
-     vowels.indexOf(curr) != -1 ? acc + 1 : acc),
-     0
-     );
-};
-console.log(countVowels("This is SOME Text"));
-/* Question 2 */
-export const runLengthEncoding : (str :string) => string = (str: string)=>{
-  const strAsArray: string[] = stringToArray(str); 
-  let counter: number = 0; //need to change
-  const newStr:string= strAsArray.reduce((
-    (acc :string, curr: string) =>
-      {
-        if(curr==acc.charAt(acc.length-1)){
-          counter=counter+1;
-          return acc;
-        }
-        else{
-          acc=acc+counter+curr;
-          counter=1;
-          return acc;
-
-        }
-      }),
-      strAsArray[0]
-     );
-  return counter>1 ? newStr+counter: newStr;
-};
-console.log(runLengthEncoding("aaaabbbccdd"));
-/* Question 3 */
-export const isPaired :(str: string)=>boolean = (str:string)=>{
-  const strAsArray: string[] = stringToArray(str); 
-  const parentheses:string="(){}[]";
-  const filteredArray = strAsArray.filter(
-    (curr: string)=> parentheses.indexOf(curr) != -1
-  );
-  console.log(filteredArray);
-  return false;
-};
-=======
 export const countVowels: (str: string) => number = (str: string): number => {
   const stringArr: string[] = stringToArray(str.toLowerCase());
   const vowels: String = "aeiou";
@@ -128,4 +83,3 @@ export const isPaired: (str: string) => boolean = (str: string): boolean => {
   return result.length == 0;
 };
 console.log(isPaired("This is [some[ (text)"));
->>>>>>> 8a44c19cdaf022067465c1893f390e36fd5b9b5f
