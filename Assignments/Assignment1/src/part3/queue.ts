6eac1d371f6d73216d5b72d9427e7be3ecb5e9d2
amityahav <74831037+amityahav@users.noreply.github.com>
2021-03-23 14:44:12 +0200

    DONE

--- Assignments/Assignment1/src/part3/queue.ts
+++ Assignments/Assignment1/src/part3/queue.ts
@@ -3,38 +3,16 @@ import { State, bind } from "./state";
 export type Queue = number[];
 
 export const enqueue = (x: number): State<Queue, undefined> => {
-  return (q: Queue) => {
-    const q2: Queue = q.concat([x]);
-    return [q2, undefined];
-  };
+  return (q: Queue): [Queue, undefined] => [q.concat([x]), undefined];
 };
-/*1. Dequeues a number x from the queue
-2. Enqueues 2 * x
-3. Enqueues x / 3
-4. Dequeues*/
 
 export const dequeue: State<Queue, number> = (q: Queue): [Queue, number] => {
-  const x: number = q[0];
-  const q2: Queue = q.slice(1);
-  return [q2, x];
+  return [q.slice(1), q[0]];
 };
-/*export const bind = <S, A, B>(state: State<S, A>, f: (x: A) => State<S, B>) => {
-    return (init: S) => {[7,8]
-      const [newSeed, comp] = state(init); [[7,8],6] -> [[7,8,12],undefined]
-      const newState = f(comp);  bind(enqueue(x * 2), (x: number) => enqueue(x / 3)
-      return newState(newSeed); 
-    };
-  };*/
-export const queueManip = (q: Queue): [Queue, undefined | number] => {
-  //[6,7,8]
-  //const act1and2 = bind(dequeue, (x: number) =>
-  // bind(enqueue(x * 2), (x: number) => enqueue(x / 3))
-  // );
-  const act1and2 = bind(dequeue, (x: number) =>
-    bind(enqueue(x * 2), (x: number) => enqueue(x / 3))
-  );
-  return act1and2(q);
-  //const [q2, comp] = act1and2(q); //[[7,8,12],undefind]
-  // const act2and3 = bind(enq);
+export const queueManip = (q: Queue): [Queue, number] => {
+  return bind(dequeue, (x: number) =>
+    bind(enqueue(x * 2), (u: undefined) =>
+      bind(enqueue(x / 3), (u: undefined) => dequeue)
+    )
+  )(q);
 };
-console.log(queueManip([6, 7, 8]));
--- Assignments/Assignment1/src/part3/state.ts
+++ Assignments/Assignment1/src/part3/state.ts
@@ -7,15 +7,3 @@ export const bind = <S, A, B>(state: State<S, A>, f: (x: A) => State<S, B>) => {
     return newState(newSeed);
   };
 };
-
-// ==> [ 3, 6 ]
-/*const f = (x: A) => {
-  // x is comp1
-  return (init: S) => { //init = newSeed
-    
-    const newSeed2 = do somthing
-    const comp2 = do something
-
-    return [newSeed2,[comp1,comp2]];
-  };
-  */
