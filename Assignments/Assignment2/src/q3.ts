b42506331d829bb3ddc6ab9ec3c236d084a72f28
gomerBGU <63105570+gomerBGU@users.noreply.github.com>
2021-04-10 15:41:01 +0300

    q3b started

--- Assignments/Assignment2/src/q3.ts
+++ Assignments/Assignment2/src/q3.ts
@@ -1,13 +1,41 @@
-import { /*ClassExp, ProcExp, */ Exp, Program } from "./L31-ast";
-import { Result, makeFailure } from "../shared/result";
+import { ClassExp, ProcExp,  Exp, Program } from "./L31-ast";
+import { Result, makeFailure, mapResult } from "../shared/result";
+import { Binding, CExp, IfExp, makeAppExp, makeIfExp, makePrimOp, makeProcExp, makeStrExp, makeVarDecl, VarDecl } from "../imp/L3-ast";
+import { first } from "../shared/list";
 
 /*
 Purpose: Transform ClassExp to ProcExp
 Signature: for2proc(classExp)
 Type: ClassExp => ProcExp
 */
-//export const class2proc = (exp: ClassExp): ProcExp =>
-    //@TODO
+//ProcExp {tag: "ProcExp"; args: VarDecl[], body: CExp[]; }
+// (define pair
+//     (lambda (a b)
+//       (lambda (msg)
+//           (if (eq? msg 'first)
+//               ((lambda () a) )
+//               (if (eq? msg 'second)
+//                      ((lambda () b) )
+//                      (if (eq? msg 'sum)
+//                          ((lambda () (+ a b)) )
+//                            #f))))))
+const reWriteClassBody = (bindings:Binding[]):CExp[] =>{
+    const ifStatment:IfExp = makeIfExp(
+        (
+            (funcName:string)=>makeAppExp(makePrimOp("eq?"),[makeStrExp(funcName)]),
+            
+
+            
+            
+        )
+
+}
+export const class2proc = (exp: ClassExp): ProcExp =>{
+    const procArgs: VarDecl[]  = exp.fields;
+    const procBody : CExp = makeProcExp([makeVarDecl("msg")],reWriteClassBody(exp.methods));
+    return makeProcExp(procArgs,[procBody]);
+}
+    
 
 /*
 Purpose: Transform L31 AST to L3 AST
